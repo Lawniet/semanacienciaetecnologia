12c99e3ea5080283797624cc8f064f2bc737bf64
Lauany Reis da Silva <laulauany@hotmail.com>
2017-05-08 17:42:31 -0300

    Update sobre.vue

--- vue/sections/sobre.vue
+++ vue/sections/sobre.vue
@@ -7,7 +7,53 @@
                          <span class="white--text">Sobre o evento</span>
                     </v-card-title>
                </v-card-row>
-               <v-card-row img="img/logo.png" height="300px" class="grey darken-4"></v-card-row>
+               <v-card-row >
+               <div class='container'>
+  <div class="row">
+    <div class="col-md-12">
+      <h1 class="text-center">SEMANA DE CIÊNCIAS E TECNOLOGIA</h1>
+<style>
+ @import url('https://fonts.googleapis.com/css?family=Faster+One|Monoton');
+  body {
+    width: 100%;
+    height: 100%;
+    background: url("https://img.gifamerica.com/d0921ca808b92e972f04e42f36f1d205_estrelas-estrelas-no-ceu-gif_498-574.gif");
+  }
+
+.container {
+  max-width: 60%;
+  float: none;
+  margin: 0 auto;
+}
+  
+  body:before {
+    width: 100%;
+    height: 100%;
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-image: -moz-linear-gradient(top left, rgba(29, 38, 113, 0.60), rgba(195, 55, 100, 0.60));
+    background-image: -ms-linear-gradient(top left, rgba(29, 38, 113, 0.60), rgba(195, 55, 100, 0.60));
+    background-image: -webkit-linear-gradient(top left, rgba(29, 38, 113, 0.60), rgba(195, 55, 100, 0.60));
+    background-image: -o-linear-gradient(top left, rgba(29, 38, 113, 0.60), rgba(195, 55, 100, 0.60));
+    background-image: linear-gradient(to bottom right, rgba(29, 38, 113, 0.60), rgba(195, 55, 100, 0.60));
+    background-size: auto;
+  }
+  
+  h1 {
+    color: #fff;
+    font-family: 'Monoton', cursive;
+    font-size: 70px;
+    text-shadow: 2px 2px 10px #1D2671, 4px 4px 15px #C33764, ;
+  }
+  </style>
+  
+    </div>
+  </div>
+</div>
+               </v-card-row>
                <v-card-text class="blue-grey darken-3 white--text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae posuere sem. Maecenas ut lobortis felis. Ut rutrum condimentum turpis, a consectetur risus hendrerit et.</p>
                </v-card-text>
